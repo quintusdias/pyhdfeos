7d1927622ed655067bc366ef470d183e001b1172
John Evans <john.g.evans@noaa.gov>
2015-01-14 11:06:44 -0500

    check if HDF4 before trying to dump zonal averages

--- pyhdfeos/command_line.py
+++ pyhdfeos/command_line.py
@@ -3,7 +3,24 @@ import argparse
 from .grids import GridFile
 from .swaths import SwathFile
 from .za import ZonalAverageFile
+from .lib import hdf
 
+def dump_zonal_average(filename):
+
+    # See if it is an HDF4 file before trying zonal averages.  This way
+    # we don't get the annoying HDF5 library error messages if/when it fails.
+    try:
+        fid = hdf.hopen(filename)
+        hdf.hclose(fid)
+        numzas = 0
+    except IOError:
+        # Ok, it's HDF5.
+        zaf = ZonalAverageFile(filename)
+        numzas = len(zaf.zas)
+        if numzas > 0:
+            print(zaf)
+
+    return numzas
 
 def dump_metadata():
 
@@ -23,10 +40,7 @@ def dump_metadata():
     if numswaths > 0:
         print(swf)
 
-    zaf = ZonalAverageFile(args.filename)
-    numzas = len(zaf.zas)
-    if numzas > 0:
-        print(zaf)
+    numzas = dump_zonal_average(args.filename)
 
     if numgrids == 0 and numswaths == 0 and numzas == 0:
         print('No HDF-EOS structures detected.')
